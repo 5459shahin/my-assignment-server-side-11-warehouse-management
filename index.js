b8ca75e5dfd24c3319997921b2b1bf3fc5785251
545n shahin <mohammadshahinkhan1992@gmail.com>
2022-05-11 22:57:13 +0800

    creat delet producct

--- index.js
+++ index.js
@@ -37,7 +37,14 @@ async function run() {
         });
 
         //delete
-        
+        app.delete('/products/:id', async(req , res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await bicycleCollection.deleteOne(query);
+            res.send(result);
+            
+
+        })
 
 
     }
