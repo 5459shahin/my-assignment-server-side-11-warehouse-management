98ccffc5afd513c0b8124480cc85876578231a01
545n shahin <mohammadshahinkhan1992@gmail.com>
2022-05-11 21:39:12 +0800

    add details

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express =  require ('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
 const cors = require("cors");
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,34 +11,48 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5dfou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
- async function run(){
-     try{
-         await client.connect();
-         const bicycleCollection=client.db('bicycle').collection('service');
 
-         app.get('/item', async (req,res)=>{
-            const query={};
-            const cursor=bicycleCollection.find(query);
-            const items=await cursor.toArray();
+
+async function run() {
+    try {
+        await client.connect();
+        const bicycleCollection = client.db('bicycle').collection('service');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = bicycleCollection.find(query);
+            const items = await cursor.toArray();
             res.send(items);
             console.log(items);
 
-         })
-         
-         
-     }
-     finally{
+        })
+        // details
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await bicycleCollection.findOne(query);
+            res.send(product);
 
-     }
-    };
-  
+
+        });
+
+        //delete
+        
+
+
+    }
+    finally {
+
+    }
+};
+
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
 
-    res.send('hello')
+    res.send('running assignment')
 
 })
- app.listen(port,()=>{
-     console.log('site is rauning on port');
- })
+app.listen(port, () => {
+    console.log('site is rauning on port');
+})
